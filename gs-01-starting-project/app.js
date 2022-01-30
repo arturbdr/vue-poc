2df430598ffee99310031c29a412bdb5056fc9b3
Artur Drummond <arturbdr@gmail.com>
2022-01-31 00:11:45 +0100

    added vue (pura js s commented)

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,24 +1,40 @@
-let app = (function () {
-    'use strict';
-    const btnAddGoal = document.getElementById("btnAddGoal");
-    const inptGoalTxt = document.getElementById("goal");
-    const ulGoals = document.getElementById("goals");
+// let app = (function () {
+//     'use strict';
+//     const btnAddGoal = document.getElementById("btnAddGoal");
+//     const inptGoalTxt = document.getElementById("goal");
+//     const ulGoals = document.getElementById("goals");
+//
+//     const onClickBtnAddGoal = () => {
+//         const userInputtedText = inptGoalTxt.value;
+//         const li = document.createElement('li');
+//         li.appendChild(document.createTextNode(userInputtedText));
+//         ulGoals.appendChild(li);
+//     };
+//
+//     const bindElements = () => {
+//         btnAddGoal.onclick = onClickBtnAddGoal;
+//     };
+//
+//     return {
+//         init: () => {
+//             bindElements();
+//         }
+//     }
+// })();
+// app.init();
 
-    const onClickBtnAddGoal = () => {
-        const userInputtedText = inptGoalTxt.value;
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(userInputtedText));
-        ulGoals.appendChild(li);
-    };
 
-    const bindElements = () => {
-        btnAddGoal.onclick = onClickBtnAddGoal;
-    };
-
-    return {
-        init: () => {
-            bindElements();
-        }
+Vue.createApp({
+    data() {
+        return {
+            userGoals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+       addGoal() {
+           this.userGoals.push(this.enteredValue)
+           this.enteredValue = ''
+       }
     }
-})();
-app.init();
+}).mount('#app');
