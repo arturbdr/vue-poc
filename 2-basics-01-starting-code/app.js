54bbb856177639455c426c6b785e819f4382027a
Artur Drummond <arturbdr@gmail.com>
2022-02-02 00:21:15 +0100

    added method to interpolation brackets

--- 2-basics-01-starting-code/app.js
+++ 2-basics-01-starting-code/app.js
@@ -4,6 +4,15 @@ const app = Vue.createApp({
             courseGoal: "Learn Vue",
             vueDocsLink: "https://vuejs.org/v2/guide/"
         }
+    },
+    methods: {
+        randomTextMessage: function () {
+            let number = Math.random();
+            if (number < 0.5) {
+                return 'Learn vue!';
+            }
+            return 'Master vue';
+        }
     }
 });
 
